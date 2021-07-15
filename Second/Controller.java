7243d566a912363af485bb482dcb141d550833e7
parkjunhwan <73221778+parkjunhwan4104@users.noreply.github.com>
2021-07-15 16:00:13 +0900

    2회차

---
+++ Second/Controller.java
@@ -0,0 +1,19 @@
+package Second;
+
+public abstract class Controller {
+	public abstract void doCommand(String cmd);
+
+	public boolean logined() {
+		if(App.loginedMember!=null) {
+
+			return true;
+		}
+		else {
+			System.out.println("로그인이 필요한 기능입니다.");	
+			return false;
+		}
+	}
+
+
+
+}
---
+++ Second/Main.java
@@ -0,0 +1,10 @@
+package Second;
+
+public class Main {
+	public static void main(String[] args) {
+		App app=new App();
+		app.start();
+		
+	}
+
+}
